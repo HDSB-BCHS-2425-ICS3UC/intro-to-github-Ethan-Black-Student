159519ea87021d011f88b24bc6c5aedd19b724be
Ethan Black [Student] <1blacketh2@hdsb.ca>
2025-02-20 14:27:33 -0500

    coding test :)

---
+++ test.py
@@ -0,0 +1,25 @@
+"""
+y=3
+
+print(y*y)
+a=(y*y)
+
+x="4"
+
+print(int(x)+1)
+b=(int(x)+1)
+
+z=(a+b)
+
+answer = int(input())
+
+if input = (14) then print(yep!)
+"""
+
+print("enter your name")
+name = input()
+print("your name is " + name)
+print("Please Confirm Y/N")
+Confirm = input()
+Y = True
+N = False
